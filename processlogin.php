0a7df10b135fbb76426b57c1f5cb1cde61051ed2
THRMills <tomhrmills@icloud.com>
2025-11-20 09:45:17 +0000

    asas

--- processlogin.php
+++ processlogin.php
@@ -1,12 +1,13 @@
 <?php
     print_r($_POST);
-    array_map("htmlspecialcharrs",$_POST);
+    array_map("htmlspecialchars",$_POST);
     include_once("connection.php");
     $stmt1= $conn->prepare("SELECT * FROM tblusers WHERE Username=:Username");
-     $stmt1->bindParam(":Username",$_POST["Username"]);
+    $stmt1->bindParam(":Username", $_POST["username"]);
     $stmt1->execute();
-    while ($row = $stmt1->fetch(PDO::FETCH_ASSOC))
-    {
-        echo($row["Forename"]." ".$row["Surname"]."<br>");
+    while ($row = $stmt1->fetch(PDO::FETCH_ASSOC)) 
+   {
+      echo($row["Forename"]." ".$row["Surname"]."<br>");
+      if 
     }
     ?>
