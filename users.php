81f1d3ad4209dae7e675c772ad09d075bf933d00
THRMills <tomhrmills@icloud.com>
2025-11-20 09:19:58 +0000

    dfgh

---
+++ users.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>          
+    <title>Add users</title>
+</head>
+
+<body>
+    <form>
+    <form action = "adduser.php" method = "POST">
+        Surname: <input type="text" name="surname"><br>
+        Forename: <input type="text" name="forename"><br>
+        Password: <input type="password" name="password"><br>
+        Year: <input type="number" name="year"><br>
+        Initial Balance: <input type="number" name="balance"><br>
+        Role: <br>
+        <input type="radio" name="role" value="pupil" checked>pupil<br>
+        <input type="radio" name="role" value="admin" >Admin<br>
+        <input type="submit" value="Add user"><br>
+    </form>
+</body>
+</html>
