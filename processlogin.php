eee73101d3c5d641c9e8158297017edb8cb0ab7c
THRMills <tomhrmills@icloud.com>
2025-11-20 09:30:29 +0000

    sdsds

--- processlogin.php
+++ processlogin.php
@@ -1,10 +1,12 @@
- <?php
-        print_r($_POST);
-        include_once("connection.php");
-        $stmt1= $conn->prepare("SELECT * FROM tblusers");
-        $stmt1->execute();
-        while ($row = $stmt1->fetch(PDO::FETCH_ASSOC))
-        {
-            echo($row["Forename"]." ".$row["Surname"]."<br>");
-        }
+<?php
+    print_r($_POST);
+    array_map("htmlspecialcharrs",$_POST);
+    include_once("connection.php");
+    $stmt1= $conn->prepare("SELECT * FROM tblusers WHERE Username=:Username");
+     $stmt1->bindParam(":Username",$_POST["Username"]);
+    $stmt1->execute();
+    while ($row = $stmt1->fetch(PDO::FETCH_ASSOC))
+    {
+        echo($row["Forename"]." ".$row["Surname"]."<br>");
+    }
     ?>
