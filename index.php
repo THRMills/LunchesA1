0b1d4989ad5d56e47a83a31b859841929d8921a4
THRMills <tomhrmills@icloud.com>
2025-11-27 21:14:28 +0000

    Add welcome message on index page

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <?php
         session_start();
         print_r($_SESSION);
+        echo isset($_SESSION["firstname"]) ? "Welcome, " . htmlspecialchars($_SESSION["firstname"]) . "!" : "Welcome, guest!";
     ?>
   <h1>Main Page</h1>
   <a href= "user.php"> add user </a><br>
