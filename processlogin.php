cb50bd5257ba338dd7e4802c63b70172c9b423ac
THRMills <tomhrmills@icloud.com>
2025-11-27 21:38:00 +0000

    dsd

--- processlogin.php
+++ processlogin.php
@@ -1,27 +1,30 @@
 <?php
-    //header("location: index.php");
-    session_start();#start session if you want to use session variables
-    print_r($_POST);
-    array_map ("htmlspecialchars",$_POST);//santitises inputs so no HTML injection
-    include_once("connection.php");
-    $stmt1= $conn->prepare("SELECT * FROM tblusers WHERE Username=:Username");
-    $stmt1->bindParam(":Username", $_POST["username"]);
-    $stmt1->execute();
-    while ($row = $stmt1->fetch(PDO::FETCH_ASSOC))
-    {
-        $hashed=$row["Password"];
-        $attempt=$_POST["password"];
-        echo ($hashed.$attempt);
-        if (password_verify($attempt,$hashed)){
-            echo("valid password");
-            $_SESSION["firstname"]=$row["Forename"];
-            $_SESSION["loggedinuser"]=$row["UserID"];
-            $_SESSION["role"]=$row["Role"];
-        }
-        else{
-            echo("invalid password");
-        }
-        
-        
+  header("location: index.php");
+  session_start(); #starts the session if you want to use session variables
+  print_r($_POST);
+  array_map("htmlspecialchars",$_POST);
+  include_once("connection.php");
+  $stmt1= $conn->prepare("SELECT * FROM tblusers WHERE Username=:Username");
+  $stmt1->bindParam(":Username", $_POST["username"]);
+  $stmt1->execute();
+    while ($row = $stmt1->fetch(PDO::FETCH_ASSOC)) s
+   {
+    $hashed=$row["Password"];
+    $attempt=$_POST["password"];
+    echo($hashed.$attempt);
+    
+    print_r($row);
+    if($row["Password"]==$_POST["password"]){
+      echo("valid password");
+      $_SESSION["firstname"]=$row["Forename"];
+      $_SESSION["loggedinuser"]=$row["UserID"];
+      $_SESSION["role"]=$row["Role"];
+
+   }
+    else{
+        echo("Invalid password");
     }
+         
+   }
+
     ?>
