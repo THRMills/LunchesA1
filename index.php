3b7dd58f67a38b673e726a7972245046ed67170a
THRMills <tomhrmills@icloud.com>
2025-11-27 09:09:55 +0000

    asas

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     
     <h1>hello World</h1>
     <a href= "user.php"> add user </a>
-    <a href= "user.php"> add user </a>
-    <a href= "user.php"> add user </a>
+    <a href= "food.php"> add user </a>
+    <a href= "login.php"> add user </a>
     
 </body>
 </html>
