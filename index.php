8ad572743fbe7ff3b00bd703df3f44356aebecd2
THRMills <tomhrmills@icloud.com>
2025-11-27 09:11:55 +0000

    asas

--- index.php
+++ index.php
@@ -5,11 +5,14 @@
 </head>
 
 <body>
-    
-    <h1>hello World</h1>
-    <a href= "user.php"> add user </a>
-    <a href= "food.php"> add user </a>
-    <a href= "login.php"> add user </a>
+    <?php
+        session_start();
+        print_r($_SESSION);
+    ?>
+  <h1>Main Page</h1>
+  <a href= "user.php"> add user </a><br>
+  <a href= "food.php"> add user </a><br>
+  <a href= "login.php"> add user </a><br>
     
 </body>
 </html>
