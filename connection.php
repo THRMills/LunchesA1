89ef7abbd9ddc58bb711fa55edc54c0b9888ed54
THRMills <tomhrmills@icloud.com>
2025-11-27 23:21:03 +0000

    zz

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername="localhost";
 $username="root";
 $password="password";
-$dbname="Lunches";
+$dbname="Lunchesa1";
 try{
     $conn= new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
